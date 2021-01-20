3b41fbdfdb7edd6b918e41e6716b252a1ecc9afc
Jays Palma <jayshobbyrc@gmail.com>
2021-01-20 19:13:23 +0800

    Hello World Code

---
+++ Tutorial 1/Hello World/Hello World/Hello World.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "My name is Jayson.\n";
+    cout << "Today is Tuesday.";
+    system("pause>0");
+}
